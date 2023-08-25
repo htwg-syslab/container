834b5365aaab25578cc1e96df10e634f60def3cc
Johannes Waidner <Johannes.Waidner@htwg-konstanz.de>
2023-08-25 14:33:36 +0200

    arm and x86 are one

--- bsys/ui/start_desktop.sh
+++ bsys/ui/start_desktop.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 ip=$(hostname --ip)
 rm -rf "/tmp/.X"*
-rm -rf "/home/syslab/.vnc/unix:*"
-cat /home/syslab/.vnc/xstartup.turbovnc
-ls /home/syslab/.vnc -al
+rm -rf "/home/bsys/.vnc/unix:*"
+cat /home/bsys/.vnc/xstartup.turbovnc
+ls /home/bsys/.vnc -al
 chmod -f 777 /tmp/.X11-unix
 # From: https://superuser.com/questions/806637/xauth-not-creating-xauthority-file (squashes complaints about .Xauthority)
-sudo -u syslab -H bash -c "touch /home/syslab/.Xauthority"
-sudo -u syslab -H bash -c "xauth generate :1 . trusted"
+sudo -u bsys -H bash -c "touch /home/bsys/.Xauthority"
+sudo -u bsys -H bash -c "xauth generate :1 . trusted"
 
-su - syslab  -c "/opt/TurboVNC/bin/vncserver -SecurityTypes None"
+su - bsys  -c "/opt/TurboVNC/bin/vncserver -SecurityTypes None"
 
-/usr/sbin/sshd -f /etc/ssh/sshd_config
 
 if [ $? -eq 0 ] ; then
-    /opt/noVNC/utils/launch.sh --vnc $ip:5901 --cert /home/syslab/.self.pem --listen 40001;
+    /opt/noVNC/utils/launch.sh --vnc $ip:5901 --cert /home/bsys/.self.pem --listen 40001;
 fi
 
